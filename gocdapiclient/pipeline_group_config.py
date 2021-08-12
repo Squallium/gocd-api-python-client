40774193c40b0cf815400da78856ebe69a1f625b
Borja Refoyo Ruiz <squallium3@gmail.com>
2021-08-12 11:38:54 +0200

    adding get, create and helper method for pipeline group config

--- gocdapiclient/pipeline_group_config.py
+++ gocdapiclient/pipeline_group_config.py
@@ -13,9 +13,14 @@ class PipelineGroupConfig(Endpoint):
 
         self._base_path = self.base_path
 
-    def all(self):
+    def get_all(self):
         return self._get('', model_class=PipelineGroupsModel)
 
+    def get(self, pipeline_group_name):
+        return self._get(pipeline_group_name, model_class=PipelineGroupsModel)
+
+    def create(self, body):
+        return self._post(body=body, model_class=PipelineGroupModel)
 
 class PipelineGroupsModel(BaseModel):
 
@@ -47,6 +52,7 @@ class PipelineGroupModel(BaseModel):
         self._links: LinkModel = None
         self.name: str = None
         self.__pipelines: [PipelineModel] = None
+        self.__authorization = {}
 
         super().__init__(data)
 
@@ -65,4 +71,64 @@ class PipelineGroupModel(BaseModel):
             for pipeline in value:
                 self.__pipelines.append(PipelineModel(pipeline))
 
+    @property
+    def authorization(self):
+        return self.__authorization
 
+    @authorization.setter
+    def authorization(self, value):
+        for key in value.keys():
+            self.__authorization[key] = AuthorizationModel(value[key])
+
+
+class AuthorizationModel(BaseModel):
+    def __init__(self, data) -> None:
+        self.users = []
+        self.roles = []
+
+        super().__init__(data)
+
+
+class PipelineGroupConfigCreateModel:
+    AUTH_VIEW = 'view'
+    AUTH_OPERATE = 'operate'
+    AUTH_ADMINS = 'admins'
+
+    def __init__(self, pipeline_group_name) -> None:
+        super().__init__()
+
+        self.pipeline_group_name = pipeline_group_name
+        self.users = {}
+        self.roles = {}
+
+    def add_auth_user(self, auth_level, user_name):
+        if auth_level not in self.users.keys():
+            self.users[auth_level] = []
+        self.users.get(auth_level, []).append(user_name)
+
+    def add_auth_role(self, auth_level, role_name):
+        if auth_level not in self.roles.keys():
+            self.roles[auth_level] = []
+        self.roles.get(auth_level, []).append(role_name)
+
+    def body(self):
+        authorization = {}
+        for key in self.users:
+            if key not in authorization.keys():
+                authorization[key] = {
+                    'users': [],
+                    'roles': []
+                }
+            authorization[key]['users'] = self.users[key]
+        for key in self.roles:
+            if key not in authorization.keys():
+                authorization[key] = {
+                    'users': [],
+                    'roles': []
+                }
+            authorization[key]['roles'] = self.roles[key]
+
+        return {
+            'name': self.pipeline_group_name,
+            'authorization': authorization
+        }
